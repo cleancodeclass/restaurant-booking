403bb784f4cc37b2adf4c06140b85674b621dc1d
youme <youme.bae@gmail.com>
2019-02-13 11:52:09 +0900

    Update BookingSchedulerTest.java
    3. Legacy Code 테스트 커버리지 100%

--- src/test/java/com/sds/cleancode/restaurant/BookingSchedulerTest.java
+++ src/test/java/com/sds/cleancode/restaurant/BookingSchedulerTest.java
@@ -5,6 +5,9 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,12 +17,23 @@ public class BookingSchedulerTest {
 	private static final int CAPACITY_PER_HOUR = 3;
 	private static final int NUMBER_OF_PEOPLE = 1;
 	private static final Customer CUSTOMER = new Customer("", "");
+	private static final Customer CUSTOMER_WITH_EMAIL = new Customer("", "", "");
+	private static final DateTime ON_THE_HOUR = new DateTime(2019, 2, 13, 9, 0);
+	private static final DateTime NOT_ON_THE_HOUR = new DateTime(2019, 2, 13, 9, 10);
 	
 	private BookingScheduler bookingScheduler;
+	
+	private List<Schedule> schedules = new ArrayList<Schedule>(); 
+	private SmsSender smsSender = new SmsSender();
+	private MailSender mailSender = new MailSender(); 
+	
 
 	@Before
 	public void setup() {
 		bookingScheduler = new BookingScheduler(CAPACITY_PER_HOUR);
+		bookingScheduler.setSchedules(schedules);
+		bookingScheduler.setSmsSender(smsSender);
+		bookingScheduler.setMailSender(mailSender);
 	}
 
 	@Test
@@ -33,10 +47,7 @@ public class BookingSchedulerTest {
 	@Test
 	public void throwExceptionWhenBookingTimeIsNotOnTheHour() {
 		// given
-		DateTime dateTime = new DateTime(2019, 2, 13, 9, 10);
-		
-		
-		Schedule schedule = new Schedule(dateTime, NUMBER_OF_PEOPLE, CUSTOMER);
+		Schedule schedule = new Schedule(NOT_ON_THE_HOUR, NUMBER_OF_PEOPLE, CUSTOMER);
 
 		try {
 			// when
@@ -51,8 +62,66 @@ public class BookingSchedulerTest {
 	@Test
 	public void scheduleIsAddedWhenBookingTimeIsOnTheHour() {
 		// given
-		DateTime dateTime = new DateTime(2019, 2, 13, 9, 0);
-		Schedule schedule = new Schedule(dateTime, NUMBER_OF_PEOPLE, CUSTOMER);
+		Schedule schedule = new Schedule(ON_THE_HOUR, NUMBER_OF_PEOPLE, CUSTOMER);
+
+		// when
+		bookingScheduler.addSchedule(schedule);
+
+		// then
+		assertThat(bookingScheduler.hasSchedule(schedule), is(true));
+	}
+	
+	@Test
+	public void scheduleIsAddedWhenBookingTimeIsSameTimeHour() {
+		Schedule schedule = new Schedule(ON_THE_HOUR, NUMBER_OF_PEOPLE, CUSTOMER);
+		bookingScheduler.addSchedule(schedule);
+		
+		Schedule newSchedule = new Schedule(ON_THE_HOUR, NUMBER_OF_PEOPLE, CUSTOMER);
+
+		// when
+		bookingScheduler.addSchedule(newSchedule);
+
+		// then
+		assertThat(bookingScheduler.hasSchedule(newSchedule), is(true));
+	}
+	
+	@Test
+	public void scheduleIsAddedWhenBookingTimeIsDifferentFromHour() {
+		// given
+		Schedule schedule = new Schedule(ON_THE_HOUR, NUMBER_OF_PEOPLE, CUSTOMER);
+		bookingScheduler.addSchedule(schedule);
+		
+		Schedule newSchedule = new Schedule(ON_THE_HOUR.plusHours(1), NUMBER_OF_PEOPLE, CUSTOMER);
+
+		// when
+		bookingScheduler.addSchedule(newSchedule);
+
+		// then
+		assertThat(bookingScheduler.hasSchedule(newSchedule), is(true));
+	}
+	
+	@Test
+	public void throwExceptionWhenCapacityPerHourIsOver() {
+		// given
+		Schedule schedule = new Schedule(ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER);
+		bookingScheduler.addSchedule(schedule);
+		
+		Schedule newSchedule = new Schedule(ON_THE_HOUR, NUMBER_OF_PEOPLE, CUSTOMER);
+
+		try {
+			// when
+			bookingScheduler.addSchedule(newSchedule);
+			fail();
+		} catch (RuntimeException e) {
+			// then
+			assertThat(e.getMessage(), is("Number of people is over restaurant capacity per hour"));
+		}
+	}
+	
+	@Test
+	public void sendEmailToCustomerWithEmailWhenScheduleIsAdded() {
+		// given
+		Schedule schedule = new Schedule(ON_THE_HOUR, NUMBER_OF_PEOPLE, CUSTOMER_WITH_EMAIL);
 
 		// when
 		bookingScheduler.addSchedule(schedule);
