be00881774335d62a5b6ba0dec371f9df8782932
SDS <SDS@DESKTOP-0T0K75S>
2019-10-16 11:05:43 +0900

    정시에 예약하지 않으면 예외발생

--- src/test/java/com/sds/cleancode/restaurant/BookingSchedulerTest.java
+++ src/test/java/com/sds/cleancode/restaurant/BookingSchedulerTest.java
@@ -4,12 +4,25 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 public class BookingSchedulerTest {
 
-	@Test
-	public void testName() throws Exception {
-		fail();
+	@Test(expected=RuntimeException.class)
+	public void 정시에_예약하지_않으면_예외발생() {
+	
+		// arrange
+		BookingScheduler BookingScheduler= new BookingScheduler(3);
+		DateTime dateTime= new DateTime(2019, 10, 16, 11, 5);
+		int numberOfPeple= 3;
+		Customer customer= new Customer("Yu", "010-1234-5678");
+		Schedule schedule= new Schedule(dateTime, numberOfPeple, customer);
+		
+		// act
+		BookingScheduler.addSchedule(schedule);
+		
+		// assert
+
 	}
 }
